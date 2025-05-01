a4834b2301a7469d56556181246cfa8af013f5e8
SaifoGames <helmysaif45@gmail.com>
2025-05-01 21:00:02 +0300

    added player physics

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class playerController : MonoBehaviour
 {
@@ -6,6 +7,10 @@ public class playerController : MonoBehaviour
     public float jumpForce = 5f;
     public float slideSpeed = 2f;
     public float leftForce = 5f;
+    public float slideFreezeTime = 0.5f;
+    public float wallPushForce = 2f;
+
+    private float originalMoveSpeed;
 
     private Rigidbody2D rb;
 
@@ -13,15 +18,19 @@ public class playerController : MonoBehaviour
     private bool canJump = false;
     private bool canMove = true;
     private bool isSliding = false;
+    private bool isFrozen = false;
+    private bool isTouchingWall = false;
+    private bool isAirborne = true;
 
     void Start()
     {
+        originalMoveSpeed = moveSpeed;
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isAirborne == false)
         {
             jumpPressed = true;
         }
@@ -29,6 +38,17 @@ public class playerController : MonoBehaviour
 
     void FixedUpdate()
     {
+
+        if (isFrozen == true)
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        else if (isFrozen == false)
+        {
+            rb.constraints = RigidbodyConstraints2D.None;
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+
         if (canMove == true)
         {
             // Handle horizontal movement
@@ -46,9 +66,13 @@ public class playerController : MonoBehaviour
         // Handle jumping
         if (jumpPressed == true && canJump == true)
         {
+            isSliding = false;
+            isFrozen = false;
+            rb.constraints = RigidbodyConstraints2D.None;
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
-            if (isSliding == true)
+            if (isTouchingWall == true)
             {
                 rb.AddForce(Vector2.left * leftForce, ForceMode2D.Impulse);
             }
@@ -59,29 +83,57 @@ public class playerController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-
+        isAirborne = false;
         if (collision.gameObject.CompareTag("Wall"))
         {
-            canMove = false;
-
-            isSliding = true;
+            moveSpeed = wallPushForce;
+            isTouchingWall = true;
+            StartCoroutine(HandleWallCollision());
         }
 
-        // Check if the player is colliding with platform
         if (collision.gameObject.CompareTag("Platform") || collision.gameObject.CompareTag("Wall"))
         {
             canJump = true;
         }
     }
-
     void OnCollisionExit2D(Collision2D collision)
     {
+        isAirborne = true;
         // Reset jump ability when leaving the platform
         if (collision.gameObject.CompareTag("Platform") || collision.gameObject.CompareTag("Wall")) 
         {
             canMove = true;
             canJump = false;
-            isSliding = false;
+            isTouchingWall = false;
+        } 
+    }
+    private IEnumerator HandleWallCollision()
+    {
+        canMove = false;
+        isFrozen = true;
+        yield return new WaitForSeconds(slideFreezeTime);
+        isFrozen = false;
+        isSliding = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Die"))
+        {
+
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Wall trigger"))
+        {
+            moveSpeed = originalMoveSpeed;
+        }
+
+        if (other.CompareTag("Wall trigger") && isSliding == true)
+        {
+            moveSpeed = 0;
         }
     }
 }
